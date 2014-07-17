5b01d221753d288efb21547582bdcb1497c79716
excalibur <excalibur-faith@qq.com>
2014-07-18 00:50:49 +0800

    add

--- two/public/javascripts/index.js
+++ two/public/javascripts/index.js
@@ -1,7 +1,10 @@
 var socket = io();
-var pc_config = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
-var pc_constraints = {"optional": [{"DtlsSrtpKeyAgreement": true}]};
-
+var localStream;
+var iceServer = {
+    "iceServers": [{
+        "url": "stun:stun.l.google.com:19302"
+    }]
+};
 var peerConnection, localStream;
 
 
@@ -11,11 +14,16 @@ $(function(){
 		var btn = document.getElementById("submit");
 
 	  	btn.addEventListener("click",function(){
+
+
+	  		peerConnection = new RTCPeerConnection(iceServer,{optional: [{RtpDataChannels: true}]});
+			
 	  		// 提供方
 	  		if (name.value == "faith") {
 					server();
 	  		}else{
 	  				other();
+
 	  		}
 	  		btn.disabled = true;	
   		});
@@ -26,32 +34,48 @@ function server(){
 	trace("服务器启动");
 
 
-  peerConnection = new RTCPeerConnection({
-      // iceServers are used to bypass firewalls
-      // which might block our connection from user to user
-      iceServers: [
-          {url: 'stun:stun.l.google.com:19302'}
-      ]
-  },{optional: [{RtpDataChannels: true}]});
-
 	
-	
-    peerConnection.onicecandidate = onIceCandidate;
-	peerConnection.createOffer(getLocalDescription, descriptionError);;
+ 	navigator.getUserMedia({video: true,audio: true},
+          function(stream) {
 
+          	localStream = stream;
+          	// var video = document.createElement("video");
+          	// attachMediaStream(video, stream);
+
+          	// document.body.appendChild(video);
+          	// video.play();
+
+            peerConnection.addStream(localStream);
+			
+			peerConnection.createOffer(createOffer, function(err) {
+              // Handle create offer error.
+              console.log("createOffer error");
+          }, {
+              mandatory: {  // Media constrains.
+                  OfferToReceiveAudio: true,
+                  OfferToReceiveVideo: true
+              }
+          });
+
+          },
+          function(error) {
+            console.log("Video capture error: ", error.code);
+          });
+      
+peerConnection.onicecandidate = onIceCandidate;
+	
 }
 
 
 function other(){
 	trace("用户连接");
-
-  peerConnection = new RTCPeerConnection({
-      // iceServers are used to bypass firewalls
-      // which might block our connection from user to user
-      iceServers: [
-          {url: 'stun:stun.l.google.com:19302'}
-      ]
-  },{optional: [{RtpDataChannels: true}]});
+	
+	
+ 	
+ 	peerConnection.onaddstream = function(event){
+ 		trace("接收到流");
+ 	};
+	peerConnection.onicecandidate = onIceCandidate;
 }
 
 
@@ -70,28 +94,61 @@ function onIceCandidate(event) {
 
 function sendMessage(message) {
 	trace("======sendMessage==========");
-	var msgString = JSON.stringify(message);
-	trace(msgString);
-	socket.emit('candidate', msgString);
+	// var msgString = JSON.stringify(message);
+	// trace(msgString);
+	socket.emit('candidate', message);
 }
 
  socket.on('candidate', function(msg){
-    console.log('message: ' + msg);
-
-    if (peerConnection) {
+    console.log('接受到房间信息');
+    
+    if (!peerConnection) {
     	return;
     };
     
 
+    if (msg.type == "candidate") {
+    	console.log("candidate");
+    	peerConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
+    }else if (msg.type == "offer"){
+    	console.log("offer");
+    	peerConnection.setRemoteDescription(new RTCSessionDescription(sdp));
+    	peerConnection.createAnswer(createAnswer,function(err) {
+         // Failed to set description. Handle error. 
+         console.log("createAnswer error");
+         console.log(err);
+     },{
+        mandatory: { // Media constrains.
+            OfferToReceiveAudio: true,
+            OfferToReceiveVideo: true
+        }
+    });
+    }else if(msg.type == "answer"){
+			console.log("answer");
+    }
+
   });
 
- function getLocalDescription(desc) {
-    //local peer set local description
-    peerConnection.setLocalDescription(desc);
-    trace("Remote peer get offer from local peer. \n" + desc.sdp);
-    //remote peer set remote description
-    peerRemoteConnection.setRemoteDescription(desc);
- 
-    peerRemoteConnection.createAnswer(getRemoteDescription, descriptionError);
+function createOffer(desc){
+	console.log(desc);
+	peerConnection.setLocalDescription(desc,function(){
+		socket.emit('candidate', desc);
+	}, function(err) {
+              // Handle create offer error.
+               console.log("error");
+          });
+
 }
- 
+
+
+function createAnswer(desc){
+	console.log("createAnswer");
+	peerConnection.setLocalDescription(desc,function(){
+		socket.emit('candidate', desc);
+	}, function(err) {
+              // Handle create offer error.
+              console.log("error");
+          });
+	
+	
+}
